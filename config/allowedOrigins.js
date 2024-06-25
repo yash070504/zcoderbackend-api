079ed5069bb1bc3b1070e2b7c1ec93c4ed921828
yash070504 <yashiitg376@gmail.com>
2024-06-25 16:29:43 +0530

    ready

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+  'https://localhost:3500',
+   'http://localhost:5173',
+   "http://localhost:5173",
+   'https://zcoder-frontend-6n2olqchj-yash-kumars-projects-01acef68.vercel.app/'
+
+]
+
+module.exports = allowedOrigins ;
